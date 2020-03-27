d9187d74c790962faed1a1a0e549be78b265118a
pauloepifani <paulorepifani@gmail.com>
2020-03-27 09:26:49 -0300

    Feature: connect application to api

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Link } from 'react-router-dom';
-
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -11,15 +11,39 @@ import heroesImg from '../../assets/heroes.png';
 
 
 export default function Login() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        try {
+            const response = await api.post('sessions', {id});
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('profile');
+        } catch (err) {
+            alert(err);
+        }
+    }
+
+
     return (
         <div className="login-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Hero" />
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu login</h1>
 
-                    <input placeHolder="Sua ID" />
+                    <input
+                        placeHolder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
+
                     <button className="button" type="submit">Entrar</button>
 
                     <Link className="back-link" to="/register">
